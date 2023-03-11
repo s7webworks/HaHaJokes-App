a183a7e4de421fc5c07abdb7a8a89818fa1d00ec
scott <scott@PAGE_FAULT>
2023-03-10 23:36:43 -0600

    Add project files.

---
+++ HaHaJokes/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using HaHaJokes.Models;
+
+namespace HaHaJokes.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<HaHaJokes.Models.Joke> Joke { get; set; }
+    }
+}
